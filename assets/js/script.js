a23f9406f4eda2ab21d2ddb0ce7cd4c17e0b7354
OrenisileDemilade <dorenisile@gmail.com>
2024-03-03 03:42:46 -0600

    third commit

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+const wrapper = document.querySelector('.wrapper');
+const facebookLink = document.querySelector('.facebook-login');
+const googleLink = document.querySelector('.google-login');
+
+google-login.addEventListener('click', () => {
+    wrapper.classList.add('active');
+});
+facebook-login.addEventListener('click', () => {
+    wrapper.classList.remove('active ');
+});
+
+
